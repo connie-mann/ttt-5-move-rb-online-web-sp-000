f0c642188fa83aca496e10e6931b2d5353beaaaf
Constance Mann <connie.mann@yahoo.com>
2021-03-01 21:12:46 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -2,6 +2,10 @@ def input_index(user_input)
   user_input.to_i - 1
 end
 
+def player_move(board, index, marker)
+  board[index] = marker
+end
+
 def move(board, position, token='X')
   board [position] = token
 end

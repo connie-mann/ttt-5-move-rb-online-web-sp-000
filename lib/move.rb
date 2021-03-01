c2ee0956dc083fc55fde4237063416297770f324
Constance Mann <connie.mann@yahoo.com>
2021-03-01 21:00:34 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,6 +6,11 @@ def move(board, position, token='X')
   board [position] = token
 end
 
-def board
-  [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+def position_taken?(board, index)
+if (board[index] == " ") || (board[index] == "") || (board[index]
+== nil)
+return false
+else
+return true
+  end
 end
